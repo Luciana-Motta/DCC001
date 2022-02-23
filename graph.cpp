d2ba6dcd497e7e7aba97e2073f7ecf3db901b239
Luciana-Motta <70346081+Luciana-Motta@users.noreply.github.com>
2022-02-23 02:03:14 -0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -160,17 +160,3 @@ void printArray(int* array, int n){
 	}
 	cout << "]" << endl;
 }
-
-int main(){
-	Graph g(5, 10);
-	g.printGraph();
-	int** matrix = g.getMatrix();
-	printMatrix(matrix, g.n);
-	int* array = g.getBinaryArray();
-	printArray(array, (int)g.n*(g.n-1)/2);
-	int* compacted = g.getCompactedArray();
-	printArray(compacted, g.compSize);
-	int** genMatrix = g.genMatrix(compacted);
-	printMatrix(genMatrix, g.n);
-	return 0;
-}
